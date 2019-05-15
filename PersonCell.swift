d1a13ad949de7a14e0538541ea213ebe2e6e58a8
BJ <bj@zo0d.hitronhub.home>
2019-05-14 20:49:17 -0400

    Done storyboard

---
+++ PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  namestofaces
+//
+//  Created by BJ on 2019-05-14.
+//  Copyright © 2019 BJ. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
